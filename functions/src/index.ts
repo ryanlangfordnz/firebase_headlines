ce19db55f4faf18838630973ac3a8916c1d376e7
ryan <bookwyrm31415@gmail.com>
2021-07-21 23:20:32 +1200

    scraper working, need to seperate out article info

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,19 +5,31 @@ import * as puppeteer from 'puppeteer';
 admin.initializeApp(); // needed to initialize the admin sdk
 const db = admin.firestore();
 
-export const taskRunner = functions.runWith( { memory: '2GB' }).region("australia-southeast1").pubsub
+export const scraper = functions.runWith( { memory: '2GB' }).region("australia-southeast1").pubsub
 
     .schedule('*/15 * * * *').onRun(async context => {
         const url = 'https://www.stuff.co.nz/';
+        
         const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
         const page = await browser.newPage();
         await page.goto(url);
+      
+
     
-        const title = await page.title()
     
+        let texts = await page.evaluate(() => {
+            let data = [];
+            let elements = document.getElementsByClassName('content');
+            for (var element of elements)
+                data.push(element.textContent);
+            
+            return data;
+        });
+        db.collection('logs').add({ hello: texts })
+
         await browser.close();
 
-        db.collection('logs').add({ hello: title })
+       
 
     });
 
